d686f8c9eb21f3fe4f9d31d51ccf0f43ab34aaeb
Lera <eklerka767@gmail.com>
2022-08-31 15:18:09 +0300

    Change Class

--- src/test/java/Class.java
+++ src/test/java/Class.java
@@ -1,9 +1,5 @@
 public class Class {
     public static void main(String[] args) {
-        try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("in.txt", true)))) {
-            out.println("New line append here !!");
-        }catch (IOException e) {
-            System.err.println(e);
-        }
+        System.out.println("Hello World!");
     }
 }
